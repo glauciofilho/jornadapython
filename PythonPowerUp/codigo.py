b50112e15db6228b764fd2ad8c04eec8efb0c265
Gláucio <glauciofilho@live.com>
2024-03-04 21:58:21 -0300

    main

--- PythonPowerUp/codigo.py
+++ PythonPowerUp/codigo.py
@@ -1,3 +1,13 @@
 import pyautogui
+import time
 
+pyautogui.PAUSE = 1
 pyautogui.press("win")
+pyautogui.write("chrome")
+pyautogui.press("enter")
+pyautogui.hotkey ("ctrl", "n")
+link = "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
+pyautogui.write(link)
+pyautogui.press("enter")
+time.sleep(5)
+pyautogui.click(x=2513, y=482)
